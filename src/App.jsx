bf0e8877d09f696d9f28a758b8b6c7443d7bb587
frckbrice <maebrie2017@gmail.com>
2023-12-27 19:09:40 +0100

    small change just to check branch working

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
         "Access-Control-Allow-Headers": "Content-Type",
       },
       body: JSON.stringify(data),
-      mode: "cors",
+      // mode: "cors",
     });
     if (response.ok) {
       response.json().then((data) => {
